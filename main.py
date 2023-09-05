51b35f4b9227d79e1d578f1c2e816ff8f53dc8c3
Rogendo <peterrogendo1999@gmail.com>
2023-09-05 15:04:31 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.theme_mode = "light"
 
     def check_item_clicked(e):
         e.control.checked = not e.control.checked
@@ -37,6 +38,30 @@ def main(page: ft.Page):
         ],
     )
     page.add(
+        ft.Card(
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.ListTile(
+                            leading=ft.Icon(ft.icons.ALBUM),
+                            title=ft.Text("Grocery Delivery, Mombasa"),
+                            subtitle=ft.Text(
+                                "pickup groceries at Naivas Supermarket..."
+                            ),
+                            # subtitle=ft.Text("Deadline at November 10, 2023 10:00 am"),
+                        ),
+                        ft.Row(
+                            [ft.TextButton("View"), ft.TextButton("Confirm")],
+                            alignment=ft.MainAxisAlignment.END,
+                        ),
+                    ]
+                ),
+                width=400,
+                padding=10,
+            )
+        ),
+        
+    
     ft.Row(
         [
             ft.Container(
