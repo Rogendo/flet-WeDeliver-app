342579f9880aaee12bb6996a696c614ddd562c2d
Peter <62094358+Rogendo@users.noreply.github.com>
2023-09-12 22:57:50 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import flet
+from flet import *
+
+def main(page:Page):
+  page.title=("We.Deliver")
+  page.padding=0
+  page.spacing=0
+
+  page.add(
+
+  #appbar
+    
+
+  #navigation bar
+
+  
+
+  )
+
+flet.app(target=main)
